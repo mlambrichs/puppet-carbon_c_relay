cd14c3acbb9343004679aa46285eab0d072851c3
Andrew Choo <achoo@vonage.com>
2016-08-11 11:21:32 -0400

    rename $limits_file_template -> $limits_template

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,25 +1,25 @@
 # == Class carbon_c_relay::service
 #
 class carbon_c_relay::service (
-  $config_file          = $carbon_c_relay::config_file,
-  $group                = $carbon_c_relay::group,
-  $limit_as             = $carbon_c_relay::limit_as,
-  $limit_cpu            = $carbon_c_relay::limit_cpu,
-  $limit_fsize          = $carbon_c_relay::limit_fsize,
-  $limit_nofile         = $carbon_c_relay::limit_nofile,
-  $limit_nproc          = $carbon_c_relay::limit_nproc,
-  $limits_file          = $carbon_c_relay::limits_file,
-  $limits_file_template = $carbon_c_relay::limits_file_template,
-  $log_dir              = $carbon_c_relay::log_dir,
-  $pid_dir              = $carbon_c_relay::pid_dir,
-  $pid_file             = $carbon_c_relay::pid_file,
-  $service_enable       = $carbon_c_relay::service_enable,
-  $service_ensure       = $carbon_c_relay::service_ensure,
-  $service_file         = $carbon_c_relay::service_file,
-  $service_manage       = $carbon_c_relay::service_manage,
-  $service_name         = $carbon_c_relay::service_name,
-  $service_template     = $carbon_c_relay::service_template,
-  $user                 = $carbon_c_relay::user,
+  $config_file      = $carbon_c_relay::config_file,
+  $group            = $carbon_c_relay::group,
+  $limit_as         = $carbon_c_relay::limit_as,
+  $limit_cpu        = $carbon_c_relay::limit_cpu,
+  $limit_fsize      = $carbon_c_relay::limit_fsize,
+  $limit_nofile     = $carbon_c_relay::limit_nofile,
+  $limit_nproc      = $carbon_c_relay::limit_nproc,
+  $limits_file      = $carbon_c_relay::limits_file,
+  $limits_template  = $carbon_c_relay::limits_template,
+  $log_dir          = $carbon_c_relay::log_dir,
+  $pid_dir          = $carbon_c_relay::pid_dir,
+  $pid_file         = $carbon_c_relay::pid_file,
+  $service_enable   = $carbon_c_relay::service_enable,
+  $service_ensure   = $carbon_c_relay::service_ensure,
+  $service_file     = $carbon_c_relay::service_file,
+  $service_manage   = $carbon_c_relay::service_manage,
+  $service_name     = $carbon_c_relay::service_name,
+  $service_template = $carbon_c_relay::service_template,
+  $user             = $carbon_c_relay::user,
 ) {
 
   if ! ($service_ensure in [ 'running', 'stopped' ]) {
@@ -50,7 +50,7 @@ class carbon_c_relay::service (
           group   => 'root',
           mode    => '0644',
           owner   => 'root',
-          content => template($limits_file_template),
+          content => template($limits_template),
         }
 
         file { $service_file:
